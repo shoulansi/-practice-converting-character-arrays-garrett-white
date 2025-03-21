2e4d3495b32557d74cd7a1c0860d32563c3284c4
shoulansi <garrettswhite@gmail.com>
2025-03-21 14:15:18 -0500

    final convert

--- practice-converting-character-arrays.js
+++ practice-converting-character-arrays.js
@@ -1,1 +1,17 @@
+let messageOne = " !yako eb ll'uoy dna ,gniog peeK !sgnittes fo yteirav a ni slliks gnidoc esu osla nac uoY !ti teg ll'uoy ,tsisrep uoy fi tub ,tsrif ta drah mees yam gnidoC";
+console.log(messageOne.split('').reverse().join(""))
 
+let messageTwo = "!ecitcarp htiw retteb teg ll‘uoy ,emit ekaT .tsrif ta drah leef lliw gnitirw edoc tuB";
+console.log(messageTwo.split("").reverse().join(""));
+
+let messageThree = "!elpoep rehto morf tnereffid on era uoy ,elbuort evah uoy fI .lanoisseforp a ekil leef ot evah t'nod uoY";
+console.log(messageThree.split("").reverse().join(""));
+
+let messageFour = ".rettam llits yeht ,smargorp llams etirw ylno nac uoy fI .tnemom tcefrep eht rof tiaw t'noD .yadot trats tsuJ";
+console.log(messageFour.split("").reverse().join(""));
+
+let myMessage = "You can do anything you can set your mind to, you just have to put in the work!";
+console.log(myMessage.split("").reverse().join(""));
+
+let myMessageTwo = "As hard as it may get, there is always light at the end of the tunnel";
+console.log(myMessageTwo.split("").reverse().join(""));
